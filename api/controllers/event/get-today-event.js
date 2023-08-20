2a4a2cea778f7ae1a54ad59ff6f5e789eef19634
AxelRj13 <axel.raharja1@gmail.com>
2023-08-21 00:03:11 +0700

    fix today event

--- api/controllers/event/get-today-event.js
+++ api/controllers/event/get-today-event.js
@@ -1,16 +1,46 @@
 module.exports = {
     friendlyName: 'Get active today events',
     fn: async function () {
-        let events = await sails.sendNativeQuery(`
-            SELECT e.store_id, e.name as store_name, e.artist, s.name, ($1 || e.image) as event_image, e.start_date, e.end_date
-            FROM events e
+        let result = await sails.sendNativeQuery(`
+            SELECT e.id, 
+                e.name, 
+                e.artist, 
+                s.id as store_id,
+                s.name as store_name, 
+                $2 || e.image as image, 
+                to_char(start_date, 'Dy, DD Mon YYYY') as date_start, 
+                to_char(start_date, 'HH24:MI') as time_start,
+                (
+                    CASE WHEN start_date > $3
+                    THEN to_char(start_date, 'YYYY-MM-DD')
+                    ELSE to_char($3, 'YYYY-MM-DD')
+                    END
+                ) as reservation_date,
+                (
+                    CASE WHEN (
+                            SELECT count(DISTINCT t1.id) 
+                            FROM tables t1 
+                            JOIN table_blueprints tb ON t1.table_blueprint_id = tb.id
+                            WHERE tb.store_id = s.id AND t1.status = $1 AND tb.status = $1
+                        ) = (
+                            SELECT count(DISTINCT bd.table_id) 
+                            FROM bookings b 
+                            JOIN booking_details bd ON bd.booking_id = b.id
+                            WHERE b.reservation_date = reservation_date AND b.status_order = $4
+                        )
+                    THEN 1
+                    ELSE 0
+                    END
+                ) as is_fully_booked
+            FROM events e 
             JOIN stores s ON e.store_id = s.id
-            WHERE e.status = $2 AND date(e.start_date) = $3
-            ORDER BY e.start_date
-        `, [sails.config.imagePath, 1, new Date()]);
+            WHERE e.status = $1 AND 
+                s.status = $1 AND 
+                date(e.start_date) = $3
+        `, [1, sails.config.imagePath, new Date(), 2]);
 
-        if (events.rows.length > 0) {
-            return sails.helpers.convertResult(1, '', events.rows, this.res);
+        if (result.rows.length > 0) {
+            return sails.helpers.convertResult(1, '', result.rows, this.res);
         } else {
             return sails.helpers.convertResult(0, 'Not Found', null, this.res);
         }
