ea7a89a83b75627a663c8fac0b97f593fadb2ade
AxelRj13 <axel.raharja1@gmail.com>
2024-01-07 16:08:16 +0700

    fix membership point calculation if not exist

--- api/controllers/booking/confirm-payment.js
+++ api/controllers/booking/confirm-payment.js
@@ -97,6 +97,45 @@ module.exports = {
             if (memberConfigPoint.rows.length > 0) {
                 var subtotal = parseInt(existingBookings.rows[0].discount ? (existingBookings.rows[0].subtotal - existingBookings.rows[0].discount) : existingBookings.rows[0].subtotal);
                 var calcPoint = Math.floor(subtotal / memberConfigPoint.rows[0].value);
+                // check user memberships data
+                let userMemberships = await sails.sendNativeQuery(`
+                    SELECT um.total_spent
+                    FROM user_memberships um
+                    WHERE um.member_id = $1
+                `, [memberId]);
+
+                var totalSpent = 0;
+                if (userMemberships.rows.length <= 0) {
+                    // if memberships not exist, create new one
+                    let newMembership = await sails.sendNativeQuery(`
+                        INSERT INTO user_memberships (
+                            member_id, membership_tier_id, points, total_spent, status,
+                            created_by, updated_by, created_at, updated_at
+                        ) VALUES ($1, $2, $3, $4, $5, $6, $6, $7, $7)
+                        RETURNING id
+                    `, [memberId, 1, 0, 0, 1, memberId, new Date()]);
+                } else {
+                    totalSpent = userMemberships.rows[0].total_spent;
+                }
+
+                // check membership tier eligible update
+                let membershipTier = await sails.sendNativeQuery(`
+                    SELECT mt.id
+                    FROM membership_tiers mt
+                    WHERE mt.status = $1 AND mt.total_spent_min < $2
+                    ORDER BY mt.id DESC
+                    LIMIT 1
+                `, [1, totalSpent + subtotal]);
+
+                if (membershipTier.rows.length > 0) {
+                    // update membership to the next tier
+                    await sails.sendNativeQuery(`
+                        UPDATE user_memberships
+                        SET membership_tier_id = $1
+                        WHERE member_id = $2
+                    `, [membershipTier.rows[0].id, memberId]);
+                }
+
                 await sails.sendNativeQuery(`
                     UPDATE user_memberships 
                     SET points = points + $1,
@@ -104,32 +143,6 @@ module.exports = {
                         updated_at = $3
                     WHERE member_id = $4
                 `, [calcPoint, subtotal, new Date(), memberId]);
-
-                // check membership tier eligible update
-                let userMemberships = await sails.sendNativeQuery(`
-                    SELECT um.total_spent
-                    FROM user_memberships um
-                    WHERE um.member_id = $1
-                `, [memberId]);
-
-                if (userMemberships.rows.length > 0) {
-                    let membershipTier = await sails.sendNativeQuery(`
-                        SELECT mt.id
-                        FROM membership_tiers mt
-                        WHERE mt.status = $1 AND mt.total_spent_min < $2
-                        ORDER BY mt.id DESC
-                        LIMIT 1
-                    `, [1, userMemberships.rows[0].total_spent]);
-
-                    if (membershipTier.rows.length > 0) {
-                        // update membership to the next tier
-                        await sails.sendNativeQuery(`
-                            UPDATE user_memberships
-                            SET membership_tier_id = $1
-                            WHERE member_id = $2
-                        `, [membershipTier.rows[0].id, memberId]);
-                    }
-                }
             }
 
             // update booking status to success
