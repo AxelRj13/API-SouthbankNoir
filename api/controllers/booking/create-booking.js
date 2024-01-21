b7b8fa593c62e1461483eff53c1373b65cd2a8cd
AxelRj13 <axel.raharja1@gmail.com>
2024-01-21 22:40:31 +0700

    remove logs

--- api/controllers/booking/create-booking.js
+++ api/controllers/booking/create-booking.js
@@ -254,7 +254,6 @@ module.exports = {
                 }
             } else if (paymentMethod.rows[0].payment_type == 'credit_card') {
                 // authenticate credit card
-                sails.log(payload);''
                 if (payload.card_number && payload.card_exp_month && payload.card_exp_year && payload.card_cvv) {
                     const fetch = require('node-fetch');
                     const options = {
