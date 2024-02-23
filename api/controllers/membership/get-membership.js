a45641b7c1d08b3bbe05125ed3903b7a3208a79b
AxelRj13 <axel.raharja1@gmail.com>
2024-02-24 01:05:52 +0700

    add version

--- api/controllers/membership/get-membership.js
+++ api/controllers/membership/get-membership.js
@@ -56,6 +56,7 @@ module.exports = {
       result.total_spent_max = tiers.rows[0].total_spent_max;
       result.privacy_policy_url = 'https://southbanknoir.com/privacypolicy';
       result.tnc_url = 'https://southbanknoir.com/privacypolicy';
+      result.app_version = '1.0.0';
 
       return sails.helpers.convertResult(1, '', result, this.res);
     }
